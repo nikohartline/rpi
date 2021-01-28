ba74dd2648e1d8884b5a526c4c46bc5bf11bed7f
nikohartline <nikohartline@gmail.com>
2021-01-27 22:52:53 -0800

    cleaned up file and added documentation

--- simple_upload.py
+++ simple_upload.py
@@ -6,17 +6,14 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from apiclient.http import MediaFileUpload
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive']
+# https://developers.google.com/drive/api/v3/manage-uploads#python
+# upload API documentation
 
+#### authentication flow
 creds = None
-# The file token.pickle stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
 if os.path.exists('tokens/token.pickle'):
     with open('tokens/token.pickle', 'rb') as token:
         creds = pickle.load(token)
-# If there are no (valid) credentials available, let the user log in.
 if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
@@ -24,15 +21,15 @@ if not creds or not creds.valid:
         flow = InstalledAppFlow.from_client_secrets_file(
             'credentials/credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
     with open('tokens/token.pickle', 'wb') as token:
         pickle.dump(creds, token)
+####
 
 drive_service = build('drive', 'v3', credentials=creds)
 
 file_metadata = {'name': 'helloworld.py'}
 media = MediaFileUpload('helloworld.py', mimetype='text/plain', resumable=True)
-#text/csv for data
+# text/csv for data
 # Visit https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
 # for a list of available mimetypes
 file = drive_service.files().create(body=file_metadata,
