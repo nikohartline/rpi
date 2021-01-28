a106d7aa5c047c6e7e393e4c826e139f4974a502
Niko Hartline <nikohartline@gmail.com>
2021-01-27 22:34:37 -0800

    added import of needed function from google apis

--- simple_upload.py
+++ simple_upload.py
@@ -4,6 +4,7 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from apiclient.http import MediaFileUpload
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive']
